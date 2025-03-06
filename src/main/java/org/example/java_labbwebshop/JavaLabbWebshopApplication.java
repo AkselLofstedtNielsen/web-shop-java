8f1b68bbca8049650c48889a3fc4ca0553df6d55
AkselLofstedtNielsen <aksel98@outlook.com>
2025-03-06 14:04:36 +0100

    first commit

---
+++ src/main/java/org/example/java_labbwebshop/JavaLabbWebshopApplication.java
@@ -0,0 +1,13 @@
+package org.example.java_labbwebshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLabbWebshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaLabbWebshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/java_labbwebshop/JavaLabbWebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.java_labbwebshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLabbWebshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
