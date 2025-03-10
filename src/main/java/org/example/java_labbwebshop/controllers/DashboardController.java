f89e3b653ff09461f35879f24f7eed27f20e5bd1
majkcb <michael.bergsten@consid.se>
2025-03-10 23:47:12 +0100

    - Man kan nu filtrera produkterna via kategori

--- src/main/java/org/example/java_labbwebshop/controllers/DashboardController.java
+++ src/main/java/org/example/java_labbwebshop/controllers/DashboardController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class DashboardController {
@@ -17,9 +18,14 @@ public class DashboardController {
     private ProductService productService;
 
     @GetMapping("/dashboard")
-    public String showDashboard(Model model) {
+    public String showDashboard(@RequestParam(value = "categoryId", required = false) Long categoryId, Model model) {
         model.addAttribute("categories", categoryService.getAllCategories());
-        model.addAttribute("products", productService.getAllProducts());
-        return "dashboard"; // Thymeleaf template: dashboard.html
+
+        if (categoryId != null) {
+            model.addAttribute("products", productService.getProductsByCategory(categoryId));
+        } else {
+            model.addAttribute("products", productService.getAllProducts());
+        }
+        return "dashboard";
     }
 }
